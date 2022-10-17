d33afb43383a8e7cbfafca1a46bc29ebef45526a
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-18 01:53:59 +0300

    update

--- print_functions_derived.c
+++ print_functions_derived.c
@@ -1,27 +1,31 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 /**
- * print_unsignedToBinary - prints an integer
- * @arg: argument: 0.
- * Return: 0.
+ * print_unsignedToBinary - prints an integer.
+ * @arg: argument
+ * Return: 0
  */
 int print_unsignedToBinary(va_list arg)
 {
-	unsigned int n = va_arg(arg, unsigned int);
-	unsigned int printed;
 
-	print_binary(n, &printed);
+unsigned int n = va_arg(arg, unsigned int);
+unsigned int printed;
 
-	return (printed);
+print_binary(n, &printed);
+print_binary(n, &printed);
+
+return (printed);
 }
 
+
 /**
- * print_oct - prints number in octal base
- * @arg: lit containing octal number to be printed
+ * print_oct - prints number in octal base.
+ * @arg: list containing octal number to be printed
  * Return: number of octals printed
  */
+
 int print_oct(va_list arg)
 {
 	unsigned int num = va_arg(arg, unsigned int);
@@ -30,9 +34,7 @@ int print_oct(va_list arg)
 	int i, j, charPrinted = 0;
 
 	if (num == 0)
-	{
 		return (_putchar('0'));
-	}
 	for (copy = num; copy != 0; j++)
 	{
 		copy = copy / 8;
@@ -40,11 +42,13 @@ int print_oct(va_list arg)
 	octa = malloc(j);
 	if (!octa)
 		return (-1);
+
 	for (i = j - 1; i >= 0; i--)
 	{
 		octa[i] = num % 8 + '0';
 		num = num / 8;
 	}
+
 	for (i = 0; i < j && octa[i] == '0'; i++)
 		;
 	for (; i < j; i++)
@@ -55,10 +59,11 @@ int print_oct(va_list arg)
 	free(octa);
 	return (charPrinted);
 }
+
 /**
- * print_unsignedIntToHex - prints unsigned int to hexadecimal
+ * print_unsignedIntToHex - prints unsigned int to hexadecimal.
  * @num: number to print
- * @_case: letter 'a' on the case to print it (upper or lower)
+ * @_case: letter `a` on the case to print it (upper or lower)
  * Return: number or char printed
  */
 int print_unsignedIntToHex(unsigned int num, char _case)
@@ -68,7 +73,8 @@ int print_unsignedIntToHex(unsigned int num, char _case)
 	char *numhex;
 
 	for (num2 = num; num2 != 0; nbrCharacters++, num2 /= 16)
-		;
+	;
+
 	numhex = malloc(nbrCharacters);
 	for (i = 0; num != 0; i++)
 	{
