d9712122885f7f079466a8af8d2418d8ff9eabe9
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-17 05:51:49 -0700

    header file

--- main.h
+++ main.h
@@ -4,12 +4,24 @@
 #include <stdarg.h>
 
 int _putchar(char c);
+
 int _printf(const char *format, ...);
+
 int print_char(va_list arg);
+
 int print_str(va_list arg);
+
 int print_percent(void);
+
 int print_int(va_list arg);
+
 int print_unsigned(va_list arg);
+
+void print_binary(unsigned int n, unsigned int *printed);
+
+int print_unsignedToBinary(va_list arg);
+
+int print_unsignedIntToHex(unsigned int num, char _case);
 
 /**
  * struct SpecifiersStruct - structure definition of a printTypeStruct
