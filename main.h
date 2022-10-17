e8c2e885d1642e696d8d31b352d221607cdca9f2
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-18 01:34:30 +0300

    update

--- main.h
+++ main.h
@@ -5,21 +5,24 @@
 #include <stdio.h>
 
 int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
+
 int _printf(const char *format, ...);
+
 int print_char(va_list arg);
-int print_str(va_list arg);
+
+//int print_str(va_list arg);
+
 int print_percent(void);
-void print_binary(unsigned int n, unsigned int* printed);
+
+int print_int(va_list arg);
+
+int print_unsigned(va_list arg);
+
+void print_binary(unsigned int n, unsigned int *printed);
 
 int print_unsignedToBinary(va_list arg);
-int print_oct(va_list arg);
+
 int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR (va_list arg);
 
 /**
  * struct SpecifiersStruct - structure definition of ai printTypeStruct
