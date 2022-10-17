e0b3e024ee7190772682fc40cf71633c65b3faf1
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-18 01:49:38 +0300

    update

--- main.h
+++ main.h
@@ -4,35 +4,33 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include <stdarg.h>
+
 int _putchar(char c);
-
-int _printf(const char *format, ...);
-
-int print_char(va_list arg);
-
-//int print_str(va_list arg);
-
-int print_percent(void);
-
 int print_int(va_list arg);
-
 int print_unsigned(va_list arg);
-
-void print_binary(unsigned int n, unsigned int *printed);
-
+int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int* printed);
 int print_unsignedToBinary(va_list arg);
-
+int print_oct(va_list arg);
 int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR (va_list arg);
 
 /**
- * struct SpecifiersStruct - structure definition of ai printTypeStruct
- * @con_Specifiers: conversion Specifiers
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
  * @printer: function to print
  */
-typedef struct SpecifiersStruct
+typedef struct identifierStruct
 {
-	char *con_Specifiers;
-	int (*printer)(va_list);
-} printTypeS;
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
 #endif
