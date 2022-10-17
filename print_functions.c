1b7bd414f307b8f645fd012e937aa32d30f2d9d7
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-17 05:47:37 -0700

    print function

--- print_functions.c
+++ print_functions.c
@@ -82,13 +82,23 @@ int print_unsigned(va_list arg)
 	return (i + 1);
 }
 
+/*  -> derived function <- */
 
+/**
+ * print_unsignedToBinary - prints an integer
+ * @arg: argument
+ * Return: 0
+ */
 
+int print_unsignedToBinary(va_list arg)
+{
+	unsigned int n = va_arg(arg, unsigned int);
+	unsigned int printed;
 
-
-
-
-
+	print_binary(n, &printed);
+	print_binary(n, &printed);
+	return (printed);
+}
 
 
 
