ffa6d70528aedd7f3b1cefa450aa51a0c6434b1d
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-17 14:41:23 +0300

    update

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes the character c to 
  * @c: the character to print
  * Return: 0 on success and -1 error and errno is set appropiately
  */
