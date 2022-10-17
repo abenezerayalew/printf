98575b56d698e3995d818e1b8d9565b45be88fe5
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-17 04:42:03 -0700

    print a function

--- print_functions.c
+++ print_functions.c
@@ -33,3 +33,86 @@ for (i = 0; str[i]; i++)
 
 return (i);
 }
+
+/**
+ * print_int - prints an integer
+ * @arg: argument
+ * Return: 0
+ */
+
+int print_int(va_list arg)
+{
+	unsigned int divisor = 1, i, resp, charPrinted = 0;
+	int n = va_arg(arg, int);
+
+	if (n < 0)
+	{
+		_putchar('-');
+		charPrinted++;
+		n *= -1;
+	}
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+	for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+	return (charPrinted);
+}
+
+/**
+ * print_unsigned - prints an unigned int
+ * @arg: argument
+ * Return: 0
+ */
+int print_unsigned(va_list arg)
+{
+	int divisor = 1, i, resp;
+	unsigned int n = va_arg(arg, unsigned int);
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+	return (i + 1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
