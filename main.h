3844332f92fd5be9e7d10e1a5c08400fb6f27985
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-18 12:14:41 +0300

    test

--- main.h
+++ main.h
@@ -1,36 +1,87 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+
+#ifndef _PRINTF_H_
+#define _PRINTF_H_
+
 #include <stdarg.h>
-#include <stdlib.h>
-int prente_xx(unsigned long int num);
-int print_hex(unsigned int n, unsigned int c);
-int _printf(const char *format, ...);
-int prente_rot(va_list args);
-int prente_c(va_list args);
-int prente_s(va_list args);
-int prente_p(va_list args);
-int prente_po(va_list args);
-int _putchar(char c);
-int prente_r(va_list args);
-int prente_b(va_list args);
-int prente_d(va_list args);
-int prente_i(va_list args);
-int prente_x(va_list args);
-int prente_S(va_list args);
-int s(char c);
-int prente_X(va_list args);
-int prente_o(va_list args);
-int prente_u(va_list args);
-/**
- *struct select - match the conversion specifiers for printf
- *@select: type char pointer of the specifier
- *@foo: type pointer to function for the conversion specifier
- *
- */
-typedef struct select
-{
-char *select;
-int (*foo)(va_list args);
-} select_p;
-int finder(const char *format, select_p selects[], va_list args);
+#include <stddef.h>
+
+
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+
+/**
+ * Output a character to a custom device like UART, used by the printf() function
+ * This function is declared here only. You have to write your custom implementation somewhere
+ * \param character Character to output
+ */
+void _putchar(char character);
+
+
+/**
+ * Tiny printf implementation
+ * You have to implement _putchar if you use printf()
+ * To avoid conflicts with the regular printf() API it is overridden by macro defines
+ * and internal underscore-appended functions like printf_() are used
+ * \param format A string that specifies the format of the output
+ * \return The number of characters that are written into the array, not counting the terminating null character
+ */
+#define printf printf_
+int printf_(const char* format, ...);
+
+
+/**
+ * Tiny sprintf implementation
+ * Due to security reasons (buffer overflow) YOU SHOULD CONSIDER USING (V)SNPRINTF INSTEAD!
+ * \param buffer A pointer to the buffer where to store the formatted string. MUST be big enough to store the output!
+ * \param format A string that specifies the format of the output
+ * \return The number of characters that are WRITTEN into the buffer, not counting the terminating null character
+ */
+#define sprintf sprintf_
+int sprintf_(char* buffer, const char* format, ...);
+
+
+/**
+ * Tiny snprintf/vsnprintf implementation
+ * \param buffer A pointer to the buffer where to store the formatted string
+ * \param count The maximum number of characters to store in the buffer, including a terminating null character
+ * \param format A string that specifies the format of the output
+ * \param va A value identifying a variable arguments list
+ * \return The number of characters that COULD have been written into the buffer, not counting the terminating
+ *         null character. A value equal or larger than count indicates truncation. Only when the returned value
+ *         is non-negative and less than count, the string has been completely written.
+ */
+#define snprintf  snprintf_
+#define vsnprintf vsnprintf_
+int  snprintf_(char* buffer, size_t count, const char* format, ...);
+int vsnprintf_(char* buffer, size_t count, const char* format, va_list va);
+
+
+/**
+ * Tiny vprintf implementation
+ * \param format A string that specifies the format of the output
+ * \param va A value identifying a variable arguments list
+ * \return The number of characters that are WRITTEN into the buffer, not counting the terminating null character
+ */
+#define vprintf vprintf_
+int vprintf_(const char* format, va_list va);
+
+
+/**
+ * printf with output function
+ * You may use this as dynamic alternative to printf() with its fixed _putchar() output
+ * \param out An output function which takes one character and an argument pointer
+ * \param arg An argument pointer for user data passed to output function
+ * \param format A string that specifies the format of the output
+ * \return The number of characters that are sent to the output function, not counting the terminating null character
+ */
+int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif  // _PRINTF_H_
