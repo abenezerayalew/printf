87703662089a9782cea9214769a9a938cd0fec6e
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-18 12:00:50 +0300

    test

--- print_functions.c
+++ print_functions.c
@@ -1,124 +1,74 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
 /**
- * print_char - writes the character c to stdout
- * @arg: argument
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int print_char(va_list arg)
+*prente_c - prints characters
+*@args: arguments
+*Return: number of print
+*/
+int prente_c(va_list args)
 {
-	return (_putchar(va_arg(arg, int)));
+_putchar(va_arg(args, int));
+return (1);
+}
+/**
+*prente_s - prints string
+*@args: arguments
+*Return: number of print
+*/
+int prente_s(va_list args)
+{
+int i = 0;
+char *p;
+p = va_arg(args, char *);
+if (p == NULL)
+p = "(null)";
+for (i = 0; p[i] != '\0'; i++)
+_putchar(p[i]);
+return (i);
+}
+/**
+*prente_p - prints %
+*@args: arguments
+*Return: number of print
+*/
+int prente_p(__attribute__((unused)) va_list args)
+{
+_putchar('%');
+return (1);
 }
 
 /**
- * print_int - prints an integer.
- * @arg: argument
- * Return: 0
+ *prente_d - prints decimal
+ *@args: arguments
+ *Return: number of print
  */
-
-int print_int(va_list arg)
+int prente_d(va_list args)
 {
-
-unsigned int divisor = 1, i, resp, charPrinted = 0;
-int n = va_arg(arg, int);
-
+int a[10];
+int j, m, n, sum, count;
+n = va_arg(args, int);
+count = 0;
+m = 1000000000;
+a[0] = n / m;
+for (j = 1; j < 10; j++)
+{
+m /= 10;
+a[j] = (n / m) % 10;
+}
 if (n < 0)
 {
-	_putchar('-');
-	charPrinted++;
-	n *= -1;
+_putchar('-');
+count++;
+for (j = 0; j < 10; j++)
+a[j] *= -1;
 }
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
+for (j = 0, sum = 0; j < 10; j++)
 {
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
-return (charPrinted);
-}
-
-
-
-/**
- * print_STR - prints a string with a `S` (upper case) specificer
- * @arg: argument
- * Return: number of character printed
- */
-
-int print_STR(va_list arg)
+sum += a[j];
+if (sum != 0 || j == 9)
 {
-int i;
-char *str = va_arg(arg, char*);
-
-if (str == NULL)
-	str = "(null)";
-else if (*str == '\0')
-	return (-1);
-
-for (i = 0; str[i]; i++)
-{
-	if ((str[i] < 32 && str[i] > 0) || str[i] >= 127)
-	{
-		_putchar('\\');
-		_putchar('x');
-		if (i < 16)
-			_putchar('0');
-
-		print_unsignedIntToHex(str[i], 'A');
-	}
-	else
-		_putchar(str[i]);
+_putchar('0' + a[j]);
+count++;
 }
-
-return (i);
 }
-
-/**
- * print_str - prints a string with a `s` (lower case) specifier
- * @arg: argument
- * Return: number of character printed
- */
-
-int print_str(va_list arg)
-{
-int i;
-char *str = va_arg(arg, char*);
-
-if (str == NULL)
-	str = "(null)";
-else if (*str == '\0')
-	return (-1);
-
-for (i = 0; str[i]; i++)
-	_putchar(str[i]);
-
-return (i);
-}
-
-/**
- * print_unsigned - prints an unsigned int.
- * @arg: argument
- * Return: 0
- */
-
-int print_unsigned(va_list arg)
-{
-int divisor = 1, i, resp;
-unsigned int n = va_arg(arg, unsigned int);
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
-return (i + 1);
+return (count);
 }
