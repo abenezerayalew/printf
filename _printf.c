6d82d7e2ba849899b777936ff804577f863f588f
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-18 10:58:24 -0700

    printf function

--- _printf.c
+++ _printf.c
@@ -23,6 +23,8 @@ int printIdentifiers(char next, va_list arg)
 		{"b", print_unsignedToBinary},
 		{"o", print_oct},
 		{"x", print_hex},
+		{"X", print_HEX},
+		{"S", print_STR},
 
 		{NULL, NULL}
 	};
