315f72d45c3c30cc1f18159b4c240c5f6a3e415b
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-18 11:59:13 +0300

    test

--- print_binary.c
+++ print_binary.c
@@ -1,17 +1,32 @@
 #include "main.h"
-
 /**
- * print_binary - function that prints the binry representation of a number
- * @n: number to be printed in binary
- * @printed: printed
- * Return: hold number of characters printed
+ * prente_b - prints a binary number.
+ * @args: arguments.
+ * Return: 1.
  */
-void print_binary(unsigned int n, unsigned int *printed)
+int prente_b(va_list args)
 {
-	if (n > 1)
-	{
-		*printed += 1;
-		print_binary(n >> 1, printed);
-	}
-	_putchar((n & 1) + '0');
+int flag = 0;
+int cont = 0;
+int i, a = 1, b;
+unsigned int num = va_arg(args, unsigned int);
+unsigned int p;
+for (i = 0; i < 32; i++)
+{
+p = ((a << (31 - i)) & num);
+if (p >> (31 - i))
+flag = 1;
+if (flag)
+{
+b = p >> (31 - i);
+_putchar(b + 48);
+cont++;
+}
+}
+if (cont == 0)
+{
+cont++;
+_putchar('0');
+}
+return (cont);
 }
